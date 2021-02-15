72a4b82ad6d6973b390b20c4a9c4bbbac6e432ce
arai.takuma <arai.takuma@unipro.co.jp>
2021-02-15 19:15:17 +0900

    change

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 function header() {
     $.ajax({
-        url: "include/header.html",
+        url: "header.html",
         cache: false,
         success: function (html) {
             document.write(html);
