f024f74fe388659f665e45d718f16ba5d2b3a4cb
arai.takuma <arai.takuma@unipro.co.jp>
2021-02-19 01:19:43 +0900

    textファイルをダウンロードできるように変更

--- script.js
+++ script.js
@@ -12,16 +12,48 @@ $("#climbing-way").change(function () {
 });
 
 $(document).on("click", ".add-member", function () {
-    var $addedElement = $(this).parent().clone(true).insertAfter($(this).parent());
+    const $addedElement = $(this).parent().clone(true).insertAfter($(this).parent());
 });
 
 $(document).on("click", ".delete-member", function () {
-    var $target = $(this).parent();
+    const $target = $(this).parent();
     if ($target.parent().children(".container").length > 3) { //3というのは、CL, SL, メンバー1人、合わせて3
         $target.remove();
     }
 });
 
+$(document).on("click", "#submit", function () {
+    const blob = new Blob([makePlanText()], { type: "text/plan" });
+    const link = document.createElement('a');
+    link.href = URL.createObjectURL(blob);
+    link.download = 'climbing_plan.txt';
+    link.click();
+})
+
+const makePlanText = () => {
+    var climbingPlan = "";
+    climbingPlan += "山岳愛好会雷鳥(東京大学・お茶の水女子大学公認サークル)\n";
+    climbingPlan += document.getElementById("mountain").value + "計画書\n";
+    climbingPlan += "作成者 " + document.getElementById("planner").value + "\n";
+    climbingPlan += "■日程 " + document.getElementById("date").value + "\n";
+    climbingPlan += "■山域 " + document.getElementById("area").value + "\n";
+    memberNum = $("#member-list").children(".container").length;
+    climbingPlan += "■メンバー(" + String(memberNum) + "人)\n";
+    for (let i = 0; i < memberNum; i++) {
+        if (i === 0) {
+            const $baseTarget = $("#CL")
+            climbingPlan += "○CL"
+                + $baseTarget.find("#fullname-kanji").val() + "("
+                + $baseTarget.find("#fullname-kana").val() + "/"
+                + String($baseTarget.find("#age").val()) + "/"
+                + $baseTarget.find("#blood-type").val() + "/"
+                + String($baseTarget.find("#generation").val()) + "期)"
+                + $baseTarget.find("#belong").val() + " "
+                + $baseTarget.find("#tel").val() + "\n";
+        }
+    }
+    return climbingPlan;
+}
 
 window.addEventListener("beforeunload", (event) => {
     event.preventDefault();
